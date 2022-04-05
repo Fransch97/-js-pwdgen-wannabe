3aab40e3c5b4ef341b62ba707ac4e36605740886
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-05 14:11:49 +0200

    added some tests (extra)

---
+++ js/annoNascita.js
@@ -0,0 +1,51 @@
+console.log("funziono")
+
+let age, currentYear;
+
+age = parseInt(prompt("Inserisci la tua età"));
+
+console.log(age);
+
+currentYear = 2022;
+
+console.log(currentYear);
+
+const birthYear = currentYear - age ;
+
+console.log(birthYear)
+
+const add10 = 10;
+
+const add20 = 20;
+
+const add5 = 5;
+
+
+document.getElementById("generated-birthyear").innerHTML = 
+`
+<h1 class="text-center">BEN FATTO ORA PUOI VEDERE L'ANNO DI NASCITA</h1>
+<ul >
+<li class="text-center"><h3>I dati da te inseriti</h3></li>
+<li class="text-center">Età: ${age}</li>
+</ul>
+
+<ul >
+<li class="text-center"><h3>L'anno calcolato</h3></li>
+<li class="text-center">Anno di nascita: ${birthYear}</li>
+</ul>
+
+<ul >
+<li class="text-center"><h3>Fra 10 anni </h3></li>
+<li class="text-center">La tua età sarà: ${age+add10}</li>
+</ul>
+
+<ul >
+<li class="text-center"><h3>Fra 20 anni </h3></li>
+<li class="text-center">La tua età sarà: ${age+add20}</li>
+</ul>
+
+<ul >
+<li class="text-center"><h3>Fra 25 anni </h3></li>
+<li class="text-center">La tua età sarà: ${age+add5+add20}</li>
+</ul>
+`;
---
+++ js/calcolaIva.js
@@ -0,0 +1,25 @@
+let importo, iva;
+
+importo = parseFloat(prompt("Inserisci il montare a cui aggiungere l\'iva IMPORTANTE: inserisci solo numeri"));
+
+iva = 22;
+console.log(importo);
+
+const ImportoConIva = importo + (importo / 100 * iva);
+console.log(ImportoConIva)
+
+
+document.getElementById("generated-iva").innerHTML =
+`
+<h1 id="welcome-title" class="text-center">BENFATTO ORA PUOI VEDERE L'IMPORTO CON IVA</h1>
+
+            <ul >
+                <li class="text-center"><h3>I dati da te inseriti</h3></li>
+                <li class="text-center">Importo: ${importo}€</li>
+            </ul>
+
+            <ul >
+                <li class="text-center"><h3>L'importo con iva</h3></li>
+                <li class="text-center">Importo con iva: ${ImportoConIva}€</li>
+            </ul>
+`;
