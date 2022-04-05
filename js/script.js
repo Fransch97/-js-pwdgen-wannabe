b648732f7c289aab9a99745f399941f1ed2280a6
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-05 13:38:12 +0200

    added some style and some more

--- js/script.js
+++ js/script.js
@@ -13,4 +13,19 @@ const generatedPassword = firstName + lastName + favoriteColor ;
 
 console.log( generatedPassword);
 
-document.getElementById("generated-pw").innerHTML = generatedPassword;
+document.getElementById("generated-pw").innerHTML = 
+`
+<h1 id="welcome-title" class="text-center">BEN FATTO ORA PUOI VEDERE LA PASSWORD GENERATA PER TE</h1>
+            <ul >
+                <li class="text-center"><h3>I dati da te inseriti</h3></li>
+                <li class="text-center">Nome: ${firstName}</li>
+                <li class="text-center">Cognome: ${lastName}</li>
+                <li class="text-center">Colore preferito: ${favoriteColor}</li>
+            </ul>
+
+            <ul >
+                <li class="text-center"><h3>La password generata dal pc per te è</h3></li>
+                <li class="text-center">Password: ${generatedPassword}</li>
+            </ul>
+`
+;
