23dd15acfb2e042b3583ec99c8f98fc2977d6372
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-05 13:10:47 +0200

    first code structure and tested

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("funziono")
+
+let firstName, lastName, favoriteColor;
+
+firstName = prompt("Inserisci il tuo nome");
+lastName = prompt("Inserisci il tuo cognnome");
+favoriteColor = prompt("Inserisci il tuo colore preferito");
+
+
+console.log(firstName, lastName, favoriteColor );
