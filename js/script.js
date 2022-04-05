ec78ad9b4de15cdca6b21ff4ec84747e2de42617
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-05 13:13:23 +0200

    print the generated password inner html code finish

--- js/script.js
+++ js/script.js
@@ -8,3 +8,9 @@ favoriteColor = prompt("Inserisci il tuo colore preferito");
 
 
 console.log(firstName, lastName, favoriteColor );
+
+const generatedPassword = firstName + lastName + favoriteColor ;
+
+console.log( generatedPassword);
+
+document.getElementById("generated-pw").innerHTML = generatedPassword;
